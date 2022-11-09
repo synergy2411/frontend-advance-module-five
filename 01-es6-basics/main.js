718249e2bdf97771524d6031866a0c6770bb2487
Sumit K <synergy2411@outlook.com>
2022-11-09 12:42:05 +0530

    rest and spread added

--- 01-es6-basics/main.js
+++ 01-es6-basics/main.js
@@ -9,45 +9,163 @@
 // }
 
 
-class Person {
-    constructor(name) {
-        this.name = name;
-    }
+// class Person {
+//     constructor(name) {
+//         this.name = name;
+//     }
 
-    getDetails() {
-        return this.name;
+//     getDetails() {
+//         return this.name;
+//     }
+// }
+
+// class Employee extends Person{
+//     constructor(empId, empName){
+//         super(empName)
+//         this.empId = empId;
+//     }
+//     getEmpId(){
+//         return this.empId;
+//     }
+// }
+
+// class Student extends Person {
+
+//     #course = 'Angular';
+
+//     constructor(studId, studName) {
+//         super(studName);
+//         this.studId = studId;
+//         // this.studName = studName;
+//         this.enrolledCourse = this.#course;
+//     }
+
+//     get enrolledCourse() {
+//         return this.#course;
+//     }
+
+//     set enrolledCourse(value) {
+//         this.#course = value
+//     }
+
+//     // Overriding
+//     getDetails() {
+//         return `${this.studId} - ${super.getDetails()}  
+//         Course : ${this.enrolledCourse}`
+//     }
+// }
+
+// let john = new Student("S001", "John Doe", "Angular");
+
+// john.enrolledCourse = "React";
+
+// console.log(john.getDetails())
+
+
+
+// ARROW FUNCION LIMITATIONS
+
+// 'arguments' keyword
+
+// function add(n1, n2) {
+//     console.log(arguments);         // keyword
+// }
+// add(2, 4)
+
+// const sum = (n1, n2) => console.log(arguments)
+// sum(2, 5)
+
+// function Person (firstName, lastName){
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+// }
+
+// const Person = (firstName, lastName) => {
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+// }
+
+// const john = new Person("John", "Doe")
+
+
+
+
+
+// REST OPERATOR
+// - creates the collection from individual elements
+
+// const sum = (...args ) => {
+//     const arraySum = args.reduce((acc, curr) => acc += curr)
+
+//     return arraySum;
+// }
+
+// console.log(sum(2, 4, 5, 6, 7, 8, 9))
+
+// SPREAD OPERATOR
+// - splits the collection into individual elements
+
+
+
+// let numbers = [2, 3, 4, 5, 6];
+
+// const sum = (n1, n2, n3) => {
+//     return n1 + n2 + n3;
+// }
+
+
+// console.log(sum(...numbers));
+
+
+// let numbers = [3, 4, 5];
+
+// let newNumbers = [1, 2, ...numbers];
+
+// console.log(newNumbers);            // [1,2,[3,4,5]]
+
+
+
+let userOne = {
+    email: "john@test",
+    age: 32,
+    address: {
+        city: "Bengaluru",
+        street: "201 Main Road, Marathahalli"
     }
 }
 
 
-class Student extends Person {
+const userTwo = JSON.parse(JSON.stringify(userOne))
 
-    #course = 'Angular';
+userTwo.address.city = "Mumbai";
 
-    constructor(studId, studName) {
-        super(studName);
-        this.studId = studId;
-        // this.studName = studName;
-        this.enrolledCourse = this.#course;
-    }
+console.log(userOne);
 
-    get enrolledCourse() {
-        return this.#course;
-    }
+// let userTwo = {
+//     ...userOne,
+//     ...userOne.address
+// }
 
-    set enrolledCourse(value) {
-        this.#course = value
-    }
+// userTwo.address.city = "Hyderabad";
 
-    // Overriding
-    getDetails() {
-        return `${this.studId} - ${super.getDetails()}  
-        Course : ${this.enrolledCourse}`
-    }
-}
+// console.log(userOne);
 
-let john = new Student("S001", "John Doe", "Angular");
+// CREATES SHALLOW COPY
 
-john.enrolledCourse = "React";
+// let userTwo = {
+//     ...userOne,
+//     age: 34
+// }
 
-console.log(john.getDetails())
+// userTwo.email = "jenny@test";
+
+// userTwo.address.city = "Pune";
+
+// console.log("USER TWO : ", userTwo);
+// // { email, age : 34, address : {city , street }}
+
+
+// console.log("USER ONE : ", userOne);
+
+
+// Deep copy and Shallow Copy
