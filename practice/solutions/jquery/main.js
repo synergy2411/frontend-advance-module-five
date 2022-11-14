9a475d438fe412b8171c31824738d0066b46c766
Sumit K <synergy2411@outlook.com>
2022-11-14 17:55:57 +0530

    jquery solutions added

---
+++ practice/solutions/jquery/main.js
@@ -0,0 +1,60 @@
+$(document).ready(function () {
+
+    // Q8
+    let emailEl = $(".email");
+    $.each(emailEl, function (index, value) {
+        let emailText = $(this).text();
+        emailText = emailText.replace(" at ", "@")
+        emailText = emailText.replace(" dot ", ".")
+        $(this).replaceWith(`<a href='mailto:${emailText}'>${emailText}</a><br/>`)
+    })
+
+
+
+    // Q7
+    // let allElements = $("body *");
+    // $.each(allElements, function (index, value) {
+    //     $(value).css({
+    //         "position": "absolute",
+    //         left: (Math.random() * 300) + "px",
+    //         top: (Math.random() * 200) + "px"
+    //     });
+    // })
+
+    // Q6
+    // let count = 0;
+    // $(".q6").hover(function () {
+    //     $(".output").text("OVER")
+    //     count++;
+    //     $(".count").text("# of Rollovers : " + count)
+    // })
+    // $(".q6").mouseleave(function () {
+    //     $(".output").text("OUT")
+    //     $(".count").text("# of Rollovers : " + count)
+    // })
+
+    // Q5
+    // $("#txt-input").keyup(function () {
+    //     let value = $(this).val();
+    //     alert(value);
+    //     $(".content").text(value)
+    // })
+
+    // Q4
+    // $("p").click(function () {
+    //     $(this).css("background", "red")
+    // })
+
+    // Q3
+    // let anchorEl = document.createElement("a")
+    // $("body").find(".container").append(anchorEl)
+    // $("a").attr("href", "www.google.com")
+    // $("a").text("visit Google")
+
+    // Q2
+    // $("body").html("<h1>Second Question</h1>")
+
+    // Q1
+    // $("body").css("background", "red")
+    // $("body").css("color", "blue")
+})
